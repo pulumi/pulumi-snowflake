00055a7458d505b48e2a4810d8cef0f040775f07
aq17 <aqiu@pulumi.com>
2023-07-10 10:01:17 -0700

    update bridge api

--- provider/resources.go
+++ provider/resources.go
@@ -24,10 +24,9 @@ import (
 	snowflake "github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/provider"
 	"github.com/pulumi/pulumi-snowflake/provider/pkg/version"
 	"github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfbridge"
-	"github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfbridge/x"
+	tfbridgetokens "github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfbridge/tokens"
 	shimv2 "github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfshim/sdk-v2"
 	"github.com/pulumi/pulumi/sdk/v3/go/common/tokens"
-	"github.com/pulumi/pulumi/sdk/v3/go/common/util/contract"
 )
 
 // all of the token components used below.
@@ -156,11 +155,10 @@ func Provider() tfbridge.ProviderInfo {
 		}, MetadataInfo: tfbridge.NewProviderMetadata(metadata),
 	}
 
-	err := x.ComputeDefaults(&prov, x.TokensSingleModule("snowflake_",
-		mainMod, x.MakeStandardToken(mainPkg)))
-	contract.AssertNoErrorf(err, "failed to apply auto token mapping")
-	err = x.AutoAliasing(&prov, prov.GetMetadata())
-	contract.AssertNoErrorf(err, "auto aliasing apply failed")
+	prov.MustComputeTokens(tfbridgetokens.SingleModule("snowflake_",
+		mainMod, tfbridgetokens.MakeStandard(mainPkg)))
+
+	prov.MustApplyAutoAliases()
 
 	prov.SetAutonaming(255, "-")
 
