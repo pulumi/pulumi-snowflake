a72c50ffd8423b05af00f36c2df984a3d6cf20f1
Ian Wahbe <ian@wahbe.com>
2024-01-13 18:56:05 -0800

    lint_provider.fix

--- provider/cmd/pulumi-resource-snowflake/main.go
+++ provider/cmd/pulumi-resource-snowflake/main.go
@@ -19,9 +19,10 @@ package main
 import (
 	_ "embed"
 
+	"github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfbridge"
+
 	snowflake "github.com/pulumi/pulumi-snowflake/provider"
 	"github.com/pulumi/pulumi-snowflake/provider/pkg/version"
-	"github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfbridge"
 )
 
 //go:embed schema-embed.json
